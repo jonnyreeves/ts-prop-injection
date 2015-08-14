a77ee7ab6c34a2c7df4d7d272c960d652ebb0b6a
Jonny Reeves <github@jonnyreeves.co.uk>
2015-08-14 23:09:29 +0100

    Introduced Chai for Test Assertions
    Dramatic improvement to the readability and maintainability of the tests which is always welcome.

--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -1,22 +1,17 @@
 /// <reference path="../typings/mocha/mocha.d.ts" />
 import { invokeConstructor } from '../lib/utils';
+import { expect } from 'chai';
 
 describe('utils', () => {
     describe('.invokeConstructor', () => {
         it('should return an instance of the supplied Class', () => {
             const result : Target = invokeConstructor<Target>(Target, []);
-            if (!(result instanceof Target)) {
-                throw new Error('Expected instanceof Target Class');
-            }
+            expect(result).to.be.instanceof(Target);
         });
 
         it('should invoke the Class\' constructor with the supplied arguments', () => {
             const result : Target = invokeConstructor<Target>(Target, [ 'foo', 'bar' ]);
-            if (!(result.ctorArgs.length === 2 &&
-                result.ctorArgs[0] === 'foo' &&
-                result.ctorArgs[1] === 'bar')) {
-                throw new Error('Expected arguments to be supplied to constructor');
-            }
+            expect(result.ctorArgs).to.deep.equal([ 'foo', 'bar' ]);
         });
     });
 });
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,1 +1,2 @@
 /// <reference path="mocha/mocha.d.ts" />
+/// <reference path="chai/chai.d.ts" />
